24018470488bb70d03ba56670d10428ec026f0bb
sanjaysaravanan <sanjaysaravanan38@gmail.com>
2022-06-18 17:01:49 +0530

    2022-06-18 Zen-Class

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Index JS executed");
+
+
+const sample = document.getElementById('sample-id');
+
+console.log(sample);
---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("JS Executed");
+
+
+
+const sampleDiv = document.getElementById('sample-id');
+
+console.log(sampleDiv);
